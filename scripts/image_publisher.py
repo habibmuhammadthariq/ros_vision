7d7bd800189c6b4b023f0eb07cc5da9e1b2546fe
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2021-11-25 10:06:30 +0700

    abc

--- scripts/image_publisher.py
+++ scripts/image_publisher.py
@@ -8,6 +8,10 @@ cap = cv2.VideoCapture(0)
 print (cap.isOpened())
 bridge = CvBridge()
 
+cap.set(cv2.CAP_PROP_FPS, 5)
+fps = cap.get(5)
+print("Frames per second : {}".format(fps))
+
 def talker():
     pub = rospy.Publisher('/webcam', Image, queue_size = 1)
     rospy.init_node('ImagePublisher', anonymous = False)
@@ -21,8 +25,9 @@ def talker():
         msg = bridge.cv2_to_imgmsg(image, 'bgr8')
         pub.publish(msg)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cap.release()
+        # cv2.waitKey(10)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+            # cap.release()
 
 if __name__ == '__main__':
     try:
