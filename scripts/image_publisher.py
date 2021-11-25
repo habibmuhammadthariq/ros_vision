5ab8b2fac92cb72e0d773a365e266990097eb25a
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2021-11-25 07:16:08 +0700

    adding rospy.rate in image publisher and rospy.signal_shutdown in image subscriber

--- scripts/image_publisher.py
+++ scripts/image_publisher.py
@@ -11,7 +11,7 @@ bridge = CvBridge()
 def talker():
     pub = rospy.Publisher('/webcam', Image, queue_size = 1)
     rospy.init_node('ImagePublisher', anonymous = False)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(2.5)   #2.5 Hz, that means we serve 5 frame per seconds
 
     while not rospy.is_shutdown():
         ret, image = cap.read()
@@ -21,8 +21,7 @@ def talker():
         msg = bridge.cv2_to_imgmsg(image, 'bgr8')
         pub.publish(msg)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cap.release()
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
